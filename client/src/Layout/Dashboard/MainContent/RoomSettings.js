93c9e71c66aca28f34c0e5fb492ce9fe4d6ea619
John-Witter <mrsolginski@gmail.com>
2022-03-09 16:50:16 -0500

    controlByTab Setting GUI

--- client/src/Layout/Dashboard/MainContent/RoomSettings.js
+++ client/src/Layout/Dashboard/MainContent/RoomSettings.js
@@ -1,42 +1,104 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { RadioBtn, Button } from '../../../Components';
+import { Loading } from '../../../Components';
 import classes from './roomSettings.css';
-import { RadioBtn } from '../../../Components';
 
 class RoomSettings extends Component {
+  state = {
+    loading: true,
+    createTabsState: this.props.settings.participantsCanCreateTabs,
+    controlTabsState: this.props.settings.controlByTab,
+  }
+
   toggleCreateTabs = () => {
+    this.setState((prevState) => ({
+      createTabsState: !prevState.createTabsState
+    }));
+  }
+
+  toggleTabControl = () => {
+    this.setState((prevState) => ({
+      controlTabsState: !prevState.controlTabsState
+    }));
+  }
+
+  submit = () => {
+    const { createTabsState, controlTabsState } = this.state;
     const { roomId, settings, updateRoom } = this.props;
     const updatedSettings = { ...settings };
-    updatedSettings.participantsCanCreateTabs = !settings.participantsCanCreateTabs;
+    updatedSettings.controlByTab = controlTabsState;
+    updatedSettings.participantsCanCreateTabs = createTabsState;
     updateRoom(roomId, { settings: updatedSettings });
-  };
+  }
 
-  togglePerspective = () => {
-    const { roomId, settings, updateRoom } = this.props;
+  // togglePerspective = () => {
+  //   const { roomId, settings, updateRoom } = this.props;
 
-    const updatedSettings = { ...settings };
-    updatedSettings.participantsCanChangePerspective = !settings.participantsCanChangePerspective;
-    updateRoom(roomId, { settings: updatedSettings });
-  };
+  //   const updatedSettings = { ...settings };
+  //   updatedSettings.participantsCanChangePerspective = !settings.participantsCanChangePerspective;
+  //   updateRoom(roomId, { settings: updatedSettings });
+  // };
+
+  // toggleTabControl = () => {
+  //   const { roomId, settings, updateRoom } = this.props;
+  //   const updatedSettings = { ...settings };
+  //   updatedSettings.controlByTab = !settings.controlByTab;
+  //   updateRoom(roomId, { settings: updatedSettings });
+
+  // }
+
   render() {
+    const { createTabsState, controlTabsState, loading } = this.state;
     const { settings, owner } = this.props;
+
+    // if (loading) {
+    //   return <Loading message="Configuring your settings..." />
+    // }
+
     return owner ? (
-      <div>
-        <h2 className={classes.Heading}>Participants can create new Tabs</h2>
-        <RadioBtn
-          name="createTabs"
-          check={this.toggleCreateTabs}
-          checked={settings.participantsCanCreateTabs === true}
-        >
-          Yes
-        </RadioBtn>
-        <RadioBtn
-          name="createTabs"
-          check={this.toggleCreateTabs}
-          checked={settings.participantsCanCreateTabs === false}
-        >
-          No
-        </RadioBtn>
+      <div className={classes.SettingsContainer}>
+        <div>
+          <h2 className={classes.Heading}>Participants can create new Tabs</h2>
+          <RadioBtn
+            name="createTabs"
+            check={this.toggleCreateTabs}
+            checked={createTabsState === true}
+          >
+            Yes
+          </RadioBtn>
+          <RadioBtn
+            name="createTabs"
+            check={this.toggleCreateTabs}
+            checked={createTabsState === false}
+          >
+            No
+          </RadioBtn>
+        </div>
+
+        <div>
+          <h2 className={classes.Heading}>Enable Independent Tab Control</h2>
+          <RadioBtn
+            name="controlByTab"
+            check={this.toggleTabControl}
+            checked={controlTabsState === true}
+          >
+            Yes
+          </RadioBtn>
+          <RadioBtn
+            name="controlByTab"
+            check={this.toggleTabControl}
+            checked={controlTabsState === false}
+          >
+            No
+          </RadioBtn>
+        </div>
+        <div>
+          <Button click={this.submit}>
+            Update Room Settings
+          </Button>
+        </div>
+
         {/* Ggb perspective option set to disabled for now */}
         {/* <h2 className={classes.Heading}>
           Participants can change the perspective (GeoGebra)
@@ -65,9 +127,15 @@ class RoomSettings extends Component {
         </RadioBtn> */}
       </div>
     ) : (
-      <div>
-        <h2 className={classes.Heading}>Participants can create new Tabs</h2>
-        <div>{settings.participantsCanCreateTabs ? 'Yes' : 'No'}</div>
+      <div className={classes.SettingsContainer}>
+        <div>
+          <h2 className={classes.Heading}>Participants can create new Tabs</h2>
+          <div>{settings.participantsCanCreateTabs ? 'Yes' : 'No'}</div>
+        </div>
+        <div>
+          <h2 className={classes.Heading}>Individual Tabs can be controlled by Unique Participants</h2>
+          <div>{settings.controlByTab ? 'Yes' : 'No'}</div>
+        </div>
         {/* <h2 className={classes.Heading}>
           Participants can change the Perspective (GeoGebra)
         </h2>
